8a6f5b92b11a605b94c92a108bfe03facc32e8f4
iradbouzidi <irad.bouzidi@esprit.tn>
2021-01-10 00:29:50 +0100

    styling & navbar

--- src/pages/team.js
+++ src/pages/team.js
@@ -3,11 +3,11 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const TeamPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Home Page.</h1>
+    <SEO title="Team" />
+    <h1>Our team </h1>
   </Layout>
 )
 
-export default IndexPage
+export default TeamPage
